85b89a634c1756d8243f4bdf840c2600ed20070b
tkssoul <2495830486@qq.com>
2026-05-02 21:52:18 +0800

    整理项目目录结构

---
+++ 检测报告/htmls/js/detaildata.js
@@ -0,0 +1,77 @@
+var ReportID = 'z9gx69f4654391bae';
+var title = '基于大模型的中小学生智能编程学习平台设计与实现';
+
+                var author = '佚名';
+var uploadTime = '2026-5-1 16:33:07';
+var contentLength = 33422;
+var published_year = 1900;
+var score = 3;
+var sectionCount = 258;
+
+                    var sentenceCount = 872;
+var localScore = 3;
+
+                    var pScore = 1;
+var tScore = 2;
+var cScore = 0;
+
+                    var bScore = 0;
+var uScore = 1;
+var sbScore = 0;
+
+                    var netScore = 0;
+var paScore = 0;
+var nScore = 0;
+
+                    var fScore = 0;
+var similarResources = {"local":[{"score":"1.0","source":"\u5b66\u672f\u8054\u5408\u5e93","author":"","title":"","detail":"<b>\u6765\u6e90\uff1a<\/b>\u5b66\u672f\u8054\u5408\u5e93<br\/>","articleType":"\u5927\u5b66\u751f\u8054\u5408\u6bd4\u5bf9\u5e93"}],"net":[]};
+var baseReduceUrl = 'https://www.paperpass.com';
+
+                    var reducePayUrl = 'https://www.paperpass.com/panel/report/index?reduce_filename=dd9c502c50bb-bada-61b8-0184-ba2ed910';
+
+                    var reportViewUrl = '';
+
+                    var reduceAiPayUrl = reducePayUrl + '&type=re_ai';
+var detail_json="[]";
+var original_json="[]"
+var detail_title_info="{\"88\":{\"score\":60},\"89\":{\"score\":58},\"197\":{\"score\":43},\"473\":{\"score\":56},\"474\":{\"score\":46},\"93\":{\"score\":41},\"835\":{\"score\":42},\"99\":{\"score\":59},\"16\":{\"score\":57},\"285\":{\"score\":54},\"288\":{\"score\":49},\"486\":{\"score\":46},\"124\":{\"score\":55},\"4\":{\"score\":64},\"125\":{\"score\":76},\"565\":{\"score\":44},\"642\":{\"score\":48},\"840\":{\"score\":49},\"5\":{\"score\":74},\"126\":{\"score\":58},\"764\":{\"score\":89},\"6\":{\"score\":100},\"127\":{\"score\":44},\"369\":{\"score\":56},\"405\":{\"score\":44},\"648\":{\"score\":42},\"528\":{\"score\":56},\"21\":{\"score\":64},\"130\":{\"score\":43},\"131\":{\"score\":42},\"132\":{\"score\":50},\"298\":{\"score\":43},\"331\":{\"score\":52},\"332\":{\"score\":64},\"498\":{\"score\":51},\"499\":{\"score\":53},\"338\":{\"score\":41},\"70\":{\"score\":56},\"77\":{\"score\":51},\"140\":{\"score\":42},\"143\":{\"score\":45},\"220\":{\"score\":52},\"861\":{\"score\":57},\"543\":{\"score\":49},\"501\":{\"score\":45},\"106\":{\"score\":65},\"304\":{\"score\":52},\"546\":{\"score\":65},\"305\":{\"score\":50},\"547\":{\"score\":51},\"549\":{\"score\":40},\"629\":{\"score\":43}}";
+var section_edit_json = "[]"
+var suggestion_json = "[]"
+var proof_detail_json = "[]"
+var is_has_word = true;
+var m_is_has_word = false;
+var isSelfBuild = false;
+var is_has_text_html = true;
+var is_has_red_word_report = true;
+var is_has_double_row_word = true;
+var is_has_double_row_text = true;
+var mobile_bookmark="[]"
+ var sentenceAveLength = 38;
+ var sentenceMaxLength = 218;
+ var sentenceMinLength = 4;
+var is_has_ai_red_word_report = true;
+var is_has_ai_pdf_report = true;
+var report_source= 'paperpass';
+var index_entry= '查重报告';
+var revise_is_has_word  = false;
+var revise_is_has_text  = false;
+var check_type = 'free';
+var is_has_reduce_result = true;
+var consume_type = 'check';
+var is_org = false;
+var hasWordRewriteLevelReport = false;
+var hasWordRewriteReport = false;
+var ai_detail_json="[]"
+var ai_mobile_bookmark="[]"
+var ai_is_has_word = true;
+var m_ai_is_has_word = false;
+var aiMultipleScore = {"totalSuspectedTextRatio":70.08,"highSuspectedTextRatio":18.82,"highAndMiddleSuspectedTextRatio":67.45,"middleSuspectedTextRatio":48.63,"lowSuspectedTextRatio":2.63,"noAISuspectedTextRatio":18.53};
+var aiScore = 50.75;
+var reduce_ai= false;
+var reduce_ai_on= true;
+var is_pp_edu = false;
+var is_show_rewrite = true;
+var has_reduce_ai_word = false;
+var has_reduce_ai_mark_word = false;
+var is_has_simple_pdf_report = false;
+var is_has_ai_simple_pdf_report = false;
